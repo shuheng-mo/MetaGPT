21aede37d86c1dbd141fcf63411415b3be0e0c2c
shuheng-mo <shuheng_mo_mail@163.com>
2024-01-31 00:07:14 +0800

    Change configs

--- examples/build_customized_agent.py
+++ examples/build_customized_agent.py
@@ -45,7 +45,9 @@ class SimpleRunCode(Action):
     name: str = "SimpleRunCode"
 
     async def run(self, code_text: str):
-        result = subprocess.run(["python3", "-c", code_text], capture_output=True, text=True)
+        result = subprocess.run(
+            ["python3", "-c", code_text], capture_output=True, text=True
+        )
         code_result = result.stdout
         logger.info(f"{code_result=}")
         return code_result
